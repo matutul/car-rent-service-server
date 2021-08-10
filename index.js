e70110349781f716970e8686a3a91cd64c9165b9
matutul <ashrafujjamantutul@gmail.com>
2021-08-10 21:32:00 +0600

    checking admin api

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const uri = `mongodb+srv://admin001:${process.env.DB_PASS}@cluster0.8e42p.mongod
 app.use(express.json());
 app.use(cors());
 
-console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS);
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -20,21 +18,56 @@ app.get('/', (req, res) => {
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   console.log("Database connection errors: ", err);
-  
+
   const carsCollection = client.db(process.env.DB_NAME).collection("cars");
+  const adminsCollection = client.db(process.env.DB_NAME).collection("admins");
   // perform actions on the collection object
   console.log("Database is connected successfully");
 
   app.post('/addCar', (req, res) => {
     carsCollection.insertOne(req.body)
-    .then(result => {
-      res.send(result.acknowledged);
-    })
+      .then(result => {
+        res.send(result.acknowledged);
+      })
   })
+
   app.get('/cars', (req, res) => {
     carsCollection.find()
-    .toArray((err, cars) => {
-      res.send(cars);
+      .toArray((err, cars) => {
+        res.send(cars);
+      })
+  })
+
+  app.get('/admins', (req, res) => {
+    adminsCollection.find()
+      .toArray((err, admins) => {
+        res.send(admins);
+      })
+  })
+
+  app.post('/addAdmin', (req, res) => {
+    let alreadyAdded = false;
+    adminsCollection.find({ email: req.body.email })
+      .toArray((err, admin) => {
+        console.log(err);
+        if (admin) {
+          alreadyAdded = true;
+          res.send(admin);
+        }
+      })
+
+    if (!alreadyAdded) {
+      adminsCollection.insertOne(req.body)
+        .then(result => {
+          res.send(result.acknowledged);
+        })
+    }
+  })
+
+  app.post('/checkAdmin', (req, res) => {
+    adminsCollection.find({email: req.body.email})
+    .toArray((err, admin) => {
+      res.send(admin.length > 0);
     })
   })
 
