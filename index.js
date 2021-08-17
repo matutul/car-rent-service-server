7bf9b9d3cd6f02f3343bf2b3465f0bad1ef0756e
matutul <ashrafujjamantutul@gmail.com>
2021-08-18 05:24:05 +0600

    checkuser api for login feature

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+const admin = require('firebase-admin');
+
 require('dotenv').config();
 const port = 4000;
 const { MongoClient } = require('mongodb');
+const ObjectID = require('mongodb').ObjectId;
 const uri = `mongodb+srv://admin001:${process.env.DB_PASS}@cluster0.8e42p.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 app.use(express.json());
 app.use(cors());
+
+
+const serviceAccount = require("./molla-rent-a-car-firebase-adminsdk-sc5pw-d6fe7cd969.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 
 
 app.get('/', (req, res) => {
@@ -65,13 +76,43 @@ client.connect(err => {
   })
 
   app.post('/checkAdmin', (req, res) => {
-    adminsCollection.find({email: req.body.email})
-    .toArray((err, admin) => {
-      res.send(admin.length > 0);
-    })
+    adminsCollection.find({ email: req.body.email })
+      .toArray((err, admin) => {
+        res.send(admin.length > 0);
+      })
   })
 
+  app.post('/deleteCar', (req, res) => {
+    carsCollection.deleteOne({ _id: ObjectID(req.body.id) })
+      .then(result => {
+        res.send(result.deletedCount > 0);
+      })
+  })
+  app.post('/deleteAdmin', (req, res) => {
+    adminsCollection.deleteOne({ _id: ObjectID(req.body.id) })
+      .then(result => {
+        res.send(result.deletedCount > 0);
+      })
+  })
 });
 
+
+// firebase admin action
+// checking user by session storage's token
+app.get('/checkUsers', (req, res) => {
+  const token = req.headers.authorization;
+  const idToken = token?.split(' ')[1];
+  if (idToken) {
+    admin
+      .auth()
+      .verifyIdToken(idToken)
+      .then((decodedToken) => {
+        res.send(decodedToken);
+      })
+      .catch((error) => {
+        res.send(error);
+      });
+  }
+})
 
 app.listen(process.env.PORT || port)
